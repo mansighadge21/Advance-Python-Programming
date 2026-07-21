ff630c61f13c5215dc1a20d1199fdf80864cd250
mansighadge21 <mansighadge2106@gmail.com>
2026-07-21 10:36:05 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,14 @@
+def decorator(func):
+    def wrapper():
+        print("Before function")
+        func()
+        print("After function")
+    return wrapper
+
+
+@decorator
+def greet():
+    print("Hello!")
+
+
+greet()
